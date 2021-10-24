e88f21354ac160bd8369270f1dbffefa9a7b8046
Nico Pulido <pulido@iqo.uni-hannover.de>
2021-10-24 17:14:55 +0200

    cycle benchmark pulses without engineered pulse

---
+++ README.md
@@ -0,0 +1,39 @@
+# Pulser 2
+
+Create pulse sequences for Cycle Benchmarking and read sequences from the generated `.dc` files or from `openQASM 2.0` files.
+
+## Prerequisites
+* numpy
+* matplotlib
+* qiskit
+
+## Installation
+
+### Method 1
+Copy the file `pulser3.py` in the same folder as your script.
+
+### Method 2
+Add the following lines to the beginning of your script:
+``` python
+import sys
+import pathlib
+installation_path = pathlib.Path('/absolute/path/to/installation/folder')
+sys.path.append(installation_path)
+```
+### Method 3 (Linux only)
+Add the absolute path of the installation folder to your `$PYTHONPATH` enviroment variable.
+
+For example add to your `.bashrc`
+
+``` bash
+export PYTHONPATH="${PYTHONPATH}:/absolute/path/to/installation/folder"
+```
+
+## TODO
+
+- [x] visualize sequences of pulses ~~using `matplotlib`~~
+- [x] Introduce MS gate
+- [ ] Calculate the state
+- [ ] $\tilde B_{C(P)}^\dag$ engineered pulse
+- [ ] `compiler` to produce hfgui / artiq compatible pulses
+- [ ] `decompiler` to load the pulses into gates
---
+++ pulser3.py
@@ -0,0 +1,130 @@
+from qiskit import *
+from qiskit.circuit.library.standard_gates import XGate, x, RGate, IGate, PhaseGate, YGate, ZGate
+from qiskit.circuit import instruction
+import numpy as np
+import matplotlib.pyplot as plt
+
+pi = np.pi
+
+# Define custom gates
+# https://quantumcomputing.stackexchange.com/a/13160
+# Use via `qc.append(ms_gate, [0, 1])`
+ms_qc = QuantumCircuit(2, name='MS')
+ms_qc.h(0)
+ms_qc.cx(0, 1)
+ms_gate = ms_qc.to_instruction()
+
+B_x_qc = QuantumCircuit(1, name='B x')
+B_x_qc.append(RGate(theta=pi/2, phi=0), [0])
+B_x = B_x_qc.to_instruction()
+
+B_y_qc = QuantumCircuit(1, name='B y')
+B_y_qc.append(RGate(theta=pi/2, phi=pi/2), [0])
+B_y = B_y_qc.to_instruction()
+
+B_z_qc = QuantumCircuit(1, name='B z')
+B_z_qc.append(PhaseGate(theta=pi/2), [0])
+B_z = B_z_qc.to_instruction()
+
+R_x_qc = QuantumCircuit(1, name='R x')
+R_x_qc.append(RGate(theta=pi, phi=0), [0])
+R_x = R_x_qc.to_instruction()
+
+R_y_qc = QuantumCircuit(1, name='R y')
+R_y_qc.append(RGate(theta=pi, phi=pi/2), [0])
+R_y = R_y_qc.to_instruction()
+
+R_z_qc = QuantumCircuit(1, name='R z')
+R_z_qc.append(PhaseGate(theta=pi), [0])
+R_z = R_z_qc.to_instruction()
+
+R_mx_qc = QuantumCircuit(1, name='R -x')
+R_mx_qc.append(RGate(theta=pi, phi=0+pi), [0])
+R_mx = R_mx_qc.to_instruction()
+
+R_my_qc = QuantumCircuit(1, name='R -y')
+R_my_qc.append(RGate(theta=pi, phi=pi/2+pi), [0])
+R_my = R_my_qc.to_instruction()
+
+I_qc = QuantumCircuit(1, name='I')
+I_qc.append(IGate(), [0])
+I = I_qc.to_instruction()
+
+style={'displaycolor': {
+        'B x': ('#00ff00', '#000000'),
+        'B y': ('#00ff00', '#000000'),
+        'B x': ('#00ff00', '#000000'),
+        'MS': ('#ff5599', '#000000'),
+        }}
+
+# Make the generated sequences reproducible
+np.random.seed(999)
+
+
+class Pulser:
+    def __init__(self):
+        self.circuits = []
+
+    def get_state(qc):
+        pass
+
+    def cycle_benchmark(self, m_list=[4, 8], L=5):
+
+        # Prepare basis change combinations
+        B_operators = [B_x, B_y, B_z, I]
+        B_combinations = []
+        for Bi in B_operators:
+            for Bj in B_operators:
+                B_combinations.append((Bi, Bj))
+
+        # Paulis to choose from:
+        paulis = [R_x, R_mx, R_y, R_my, R_z, I]
+
+        # Assemble and store circuits
+        for l in range(L):
+            for k, (Bi, Bj) in enumerate(B_combinations):
+                for m in m_list:
+                    qc = QuantumCircuit(2)
+                    qc.append(Bi, [0])
+                    qc.append(Bj, [1])
+                    qc.append(np.random.choice(paulis), [0])
+                    qc.append(np.random.choice(paulis), [1])
+                    for _ in range(m):
+                        qc.append(ms_gate, [0, 1])
+                        qc.append(np.random.choice(paulis), [0])
+                        qc.append(np.random.choice(paulis), [1])
+                    self.circuits.append(qc)
+        
+        # Print circuits
+        for qc in self.circuits:
+            print(qc)
+            print()
+                    
+
+def main_custom_gates():
+    qc = QuantumCircuit(2)
+    qc.append(B_x, [0])
+    qc.append(B_y, [1])
+    qc.append(R_z, [0])
+    qc.append(R_my, [1])
+    qc.append(R_z, [1])
+    qc.append(ms_gate, [0, 1])
+    qc.append(I, [0])
+    print(qc)
+    # https://quantumcomputing.stackexchange.com/a/16921
+    
+    qc.draw('mpl', style=style)
+    plt.show()
+
+
+def main_cycle_benchmark():
+    pulser = Pulser()
+    pulser.cycle_benchmark()
+
+if __name__ == '__main__':
+    main_cycle_benchmark()
+    
+
+
+            
+
